f8449b9a2eb032a15caa0c2206c51042a4af2a2a
samirvithlani <samir.vithlani83955@gmail.com>
2023-01-06 14:02:33 +0530

    mouse

---
+++ event/Mouseevent.js
@@ -0,0 +1,20 @@
+function demo(event){
+
+    console.log(event);
+}
+
+function demo1(){
+
+    document.getElementById("text").style.backgroundColor = "yellow";
+
+}
+function demo2(){
+
+    document.getElementById("text2").style.backgroundColor = "yellow";
+
+}
+function demo4(event){
+
+    console.log(event);
+
+}
